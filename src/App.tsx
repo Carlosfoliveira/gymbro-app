e3eeaddee9101334754995c786b5ef8fc2a252cf
Carlos Oliveira [C] <carlosoliveira@rivian.com>
2024-04-12 13:13:39 -0300

    Change expo App entry

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'react-native';
 import { Navigation } from './navigation'
+import { registerRootComponent } from 'expo';
 
 export default function App() {
   return (
@@ -9,3 +10,5 @@ export default function App() {
     </>
   );
 }
+
+registerRootComponent(App);
